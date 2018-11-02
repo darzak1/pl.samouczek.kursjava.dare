c4b7609821649fe59fe2663bf96bab622b2e9824
darzak1 <darzak@gmail.com>
2018-11-02 10:36:00 +0100

    początek samouczka

--- src/pl/samouczek/kursjava/darek/Cogwheel.java
+++ src/pl/samouczek/kursjava/darek/Cogwheel.java
@@ -1,6 +1,32 @@
 package pl.samouczek.kursjava.darek;
 
-public class Cogwheel {
+import pl.samouczekprogramisty.kursjava.engine.Cogwheel;
 
+public class Engine {
+    private boolean started;
+    private Cogwheel cogwheel;
 
+    public Engine() {
+        cogwheel = new Cogwheel(4, 450);
+        started = true;
+    }
+    public Engine(Cogwheel cogwheel) {
+        this.cogwheel = cogwheel;
+    }
+
+    public void start() {
+        started = initiateStartingSequence();
+    }
+    private boolean initiateStartingSequence() {
+        return true;
+    }
+    public void stop() {
+        started = false;
+    }
+    public boolean isStarted() {
+        return started;
+    }
+    public void setStarted(boolean started) {
+        this.started = started;
+    }
 }
